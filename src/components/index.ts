d4e2ecdcb466cf9dcc435475247cfa7d95986612
yangyuan <yangyuan@kuaishou.com>
2023-09-11 21:13:56 +0800

    目录结构

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+/*
+ * @Author: yangyuan
+ * @Description: 
+ * @Date: 2023-09-11 20:48:00
+ * @LastEditTime: 2023-09-11 20:48:01
+ */
---
+++ src/types/global.d.ts
@@ -0,0 +1,10 @@
+/*
+ * @Author: yangyuan
+ * @Description: 
+ * @Date: 2023-09-11 15:14:57
+ * @LastEditTime: 2023-09-11 15:15:00
+ */
+declare module '*.module.css' {
+	const classes: { [key: string]: string };
+	export default classes;
+  }
